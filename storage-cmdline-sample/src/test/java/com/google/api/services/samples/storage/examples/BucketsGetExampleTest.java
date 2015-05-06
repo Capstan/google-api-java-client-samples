dd097953cbab6069504725f52d74d5f76c224bd8
Nathan Herring <nherring@google.com>
2015-05-06 03:09:35 -0700

    Initial test.

---
+++ storage-cmdline-sample/src/test/java/com/google/api/services/samples/storage/examples/BucketsGetExampleTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.api.services.samples.storage.examples;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import com.google.api.services.samples.storage.examples.BucketsGetExample;
+import com.google.api.services.storage.Storage;
+import com.google.api.services.storage.Storage.Buckets;
+import com.google.api.services.storage.Storage.Buckets.Get;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.io.IOException;
+
+/** Test for example class exemplifying retrieving bucket metadata. */
+public class BucketsGetExampleTest {
+
+  @Test
+  public void testGet() throws IOException {
+    final String bucketName = "mahbukkit";
+    Storage storage = Mockito.mock(Storage.class);
+    Buckets bucketsCollection = Mockito.mock(Buckets.class);
+    Get getRequest = Mockito.mock(Get.class);
+    when(storage.buckets()).thenReturn(bucketsCollection);
+    when(bucketsCollection.get(bucketName)).thenReturn(getRequest);
+    BucketsGetExample.get(storage, bucketName);
+    verify(storage).buckets();
+    verify(bucketsCollection).get(bucketName);
+    verify(getRequest).setProjection("full");
+    verify(getRequest).execute();
+    verifyNoMoreInteractions(storage, bucketsCollection, getRequest);
+  }
+
+}
