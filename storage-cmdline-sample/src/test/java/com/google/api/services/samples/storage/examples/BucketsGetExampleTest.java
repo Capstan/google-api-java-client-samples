56f7745c625425980d8942d7a1e55850d48e21c9
Nathan Herring <nherring@google.com>
2015-05-19 12:52:24 -0700

    Use PowerMockito to (try to) test main
    Currently running into unexpected issues, so the new test is ignored,
    but this gets the new maven targets and imports.

--- storage-cmdline-sample/src/test/java/com/google/api/services/samples/storage/examples/BucketsGetExampleTest.java
+++ storage-cmdline-sample/src/test/java/com/google/api/services/samples/storage/examples/BucketsGetExampleTest.java
@@ -16,28 +16,38 @@ package com.google.api.services.samples.storage.examples;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
 
+import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.services.samples.storage.util.CredentialsProvider;
 import com.google.api.services.storage.Storage;
 import com.google.api.services.storage.Storage.Buckets;
 import com.google.api.services.storage.Storage.Buckets.Get;
 import com.google.api.services.storage.model.Bucket;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.IOException;
 
 /** Test for example class exemplifying retrieving bucket metadata. */
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({CredentialsProvider.class, Storage.Builder.class})
 public class BucketsGetExampleTest {
   
   @Mock private Storage storage;
@@ -80,4 +90,18 @@ public class BucketsGetExampleTest {
     verifyNoMoreInteractions(storage, bucketsCollection, getRequest);
   }
   
+  @Ignore("NoSuchAlgorithmException: class configured for SSLContext: "
+      + "sun.security.ssl.SSLContextImpl$TLS10Context not a SSLContext")
+  @Test
+  public void testMain() throws Exception {
+    PowerMockito.mockStatic(CredentialsProvider.class);
+    when(CredentialsProvider.authorize(any(HttpTransport.class), any(JsonFactory.class)))
+        .thenReturn(Mockito.mock(Credential.class));
+    Storage.Builder storageBuilder = PowerMockito.mock(Storage.Builder.class);
+    whenNew(Storage.Builder.class).withAnyArguments().thenReturn(storageBuilder);
+    when(storageBuilder.build()).thenReturn(storage);
+    when(getRequest.execute()).thenReturn(new Bucket());
+    BucketsGetExample.main(null);
+  }
+
 }
